7999c2ed319e77a764732f52859a9f634472a379
Saidul Ali Mallick <143573754+saidulalimallick004@users.noreply.github.com>
2024-08-18 18:21:13 +0530

    Update SumOfArray.java

--- SumOfArray.java
+++ SumOfArray.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class SumOfArray {
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
@@ -13,18 +12,13 @@ public class SumOfArray {
         for(int i=0;i<n;i++){
             System.out.print("Enter Num_"+(i+1)+":");
             array[i]=sc.nextInt();
-
         }
-
         int sum=0;
 
         for(int i=0;i<n;i++){
             sum+=array[i];
-
         }
-
         System.out.println("Sum of All Elements: "+sum);
-
 
         sc.close();
     }

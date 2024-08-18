6347bbae8cbeebfda0850717197a6411fbd46407
Saidul Ali Mallick <saidulalimallick1@gmail.com>
2024-08-18 18:19:57 +0530

    created Sum of all Array Elements

---
+++ SumOfArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class SumOfArray {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter Size of Array:");
+        int n=sc.nextInt();
+
+        int[] array=new int[n];
+
+        for(int i=0;i<n;i++){
+            System.out.print("Enter Num_"+(i+1)+":");
+            array[i]=sc.nextInt();
+
+        }
+
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            sum+=array[i];
+
+        }
+
+        System.out.println("Sum of All Elements: "+sum);
+
+
+        sc.close();
+    }
+}
